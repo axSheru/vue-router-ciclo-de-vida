71d070aa8dcf1e340b2b17bcb52fb9654d052f3a
Alex Paredes <alexparedesr95@gmail.com>
2021-09-02 22:33:57 -0500

    FEAT: Se añaden estilos al layout de Star Wars.

--- src/modules/starwars/layouts/StarWarsLayout.vue
+++ src/modules/starwars/layouts/StarWarsLayout.vue
@@ -1,4 +1,12 @@
 <template>
-  <h1>SW Layout</h1>
-  <router-view></router-view>
+  <div class="starwars-layout">
+    <h1>SW Layout</h1>
+    <router-view></router-view>
+  </div>
 </template>
+
+<style scoped>
+    .starwars-layout {
+        background-color: greenyellow;
+    }
+</style>
